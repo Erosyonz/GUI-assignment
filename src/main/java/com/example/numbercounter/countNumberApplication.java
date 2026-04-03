7d366b6277501e75107d20f470ab3fc40462542b
Erosyonz <alleahidelapena@gmail.com>
2026-04-03 20:27:16 +0800

    design kunohay

--- src/main/java/com/example/numbercounter/countNumberApplication.java
+++ src/main/java/com/example/numbercounter/countNumberApplication.java
@@ -11,7 +11,7 @@ public class countNumberApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(countNumberApplication.class.getResource("numberCounter.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 340, 240);
         stage.setTitle("Number Counter");
         stage.setScene(scene);
         stage.show();
